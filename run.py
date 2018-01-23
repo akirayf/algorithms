28efa3acbad74afb15f551783500e0ac54b6af63
akirayf <156615061@qq.com>
2018-01-23 23:17:01 +0800

    Add quick sort implementation

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from sorting_algorithms.bubble_sort import bubble_sort
+from sorting_algorithms.quick_sort import quick_sort
 
 # Test bubble_sort
-num_list=[1, 7, 10, 6, 2, 5, 3, 16, 9]
-print bubble_sort(0, num_list)
-print bubble_sort(1, num_list)
+num_list_1=[1, 7, 10, 6, 2, 5, 3, 16, 9]
+num_list_2=[1, 7, 10, 6, 2, 5, 3, 16, 9]
+print bubble_sort(0, num_list_1)
+print bubble_sort(1, num_list_2)
+
+num_list_3=[1, 7, 10, 6, 2, 5, 3, 16, 9]
+num_list_4=[1, 7, 10, 6, 2, 5, 3, 16, 9]
+print quick_sort(0, num_list_3)
+print quick_sort(1, num_list_4)
--- sorting_algorithms/quick_sort.py
+++ sorting_algorithms/quick_sort.py
@@ -5,7 +5,36 @@ Implement quick sort algorithm to sory numbers in ascending/descending order
 """
 
 def quick_sort(sort_type=1, elems=[]):
-	pass
+	elemLen = len(elems)
+	if elemLen <= 1:
+		return elems
+
+	pivotIndex = 0
+	pivot = elems[pivotIndex]
+
+	headIndex  = 0
+	tailIndex = elemLen - 1
+	isRightSide = True
+	while headIndex <= tailIndex:
+		if isRightSide:
+			if (sort_type == 1 and pivot > elems[tailIndex]) or (sort_type == 0 and pivot < elems[tailIndex]):
+				elems[pivotIndex] = elems[tailIndex]
+				pivotIndex = tailIndex
+				isRightSide = False
+			tailIndex = tailIndex - 1
+		else:
+			if (sort_type == 1 and pivot < elems[headIndex]) or (sort_type == 0 and pivot > elems[headIndex]):
+				elems[pivotIndex] = elems[headIndex]
+				pivotIndex = headIndex
+				isRightSide = True
+			headIndex = headIndex + 1
+
+	if pivotIndex == elemLen - 1:
+		return quick_sort(sort_type, elems[0:pivotIndex]) + [pivot]
+	elif pivotIndex == 0:
+		return [pivot] + quick_sort(sort_type, elems[1:])
+	else:
+		return quick_sort(sort_type, elems[0:pivotIndex]) + [pivot] + quick_sort(sort_type, elems[(pivotIndex + 1):])
 
 if __name__ == '__main__':
 	quick_sort()
